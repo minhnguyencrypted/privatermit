f49c6d31d795be6f1f90ab62718317eee406d5ce
Minh Nguyen <minhnguyen1205@outlook.com>
2020-11-13 13:05:48 +0700

    Update program input method

--- Week3/problem6.c
+++ Week3/problem6.c
@@ -2,7 +2,10 @@
 
 int main() {
     int n;
-    scanf ("%d",&n);
+    do {
+	printf ("Input number: ");
+	scanf ("%d",&n);
+    } while ((n>15) || (n<1));
     for (int i=1; i<=n; i++) {
 	for (int j=n,count=-1; j<=n && j>0; j+=count) {
 	    if ((j<=i)&&(j>0))printf ("%d ",j);
