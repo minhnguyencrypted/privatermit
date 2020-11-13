0a468efafc595db4a782ad54513cea12170b6a96
Minh Nguyen <minhnguyen1205@outlook.com>
2020-11-13 12:44:39 +0700

    Initial Commit

---
+++ Week3/problem5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf ("%d",&n);
+    for (int i=n; i>0; i--) {
+	for (int j=i,count=-1; j<=i && j>0; j+=count) {
+	    printf ("%d ",j);
+	    if (j==1) count = 1;
+	}
+	printf ("\n");
+    }
+    return 0;
+}
