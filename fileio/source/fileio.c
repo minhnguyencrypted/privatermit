b260af263a1298b1812770ea153983277c56447a
Minh Nguyen <minhnguyen1205@outlook.com>
2021-01-23 18:32:46 +0700

    Bug fixed, add one function
    - Fix Missing first letter of each line in "fcopyln()" function
    - Add "fwordcounter()" function, counting word from text file.
    - Too tired to add detailed comments and cleaning code,
      I'll do it soon...

--- fileio/source/fileio.c
+++ fileio/source/fileio.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
-int fcopy (char const *finname, char const *foutname);
-int fcopyln (char const *finname, char const *foutname);
+int fcopy(char const *finname, char const *foutname);
+int fcopyln(char const *finname, char const *foutname);
+int fwordcounter(char const *finname); 
 
 int fcopy (char const *finname, char const *foutname) {
     //Open files
@@ -26,16 +28,44 @@ int fcopyln (char const *finname, char const *foutname) {
     if ((fin == NULL) || (fout == NULL)) return 0;
     
     //File(s) found
-    int lncount = 2;
-    fputs("1. ",fout);
-    for (int i=fgetc(fin); i!=EOF; i=fgetc(fin)) { 
-	fputc (i,fout);
-	if ((i == (int)'\n') && (fgetc(fin) != EOF)) {
-	    fprintf (fout,"%d. ",lncount);
-	    lncount++;
+    int lncount = 1;
+    int i = fgetc(fin);
+    if (i != EOF) fputs("1. ",fout);
+    while (!feof(fin)) {
+	if (i == '\n') {
+	    fputc(i,fout);
+	    i = fgetc(fin);
+	    if (i != EOF) fprintf (fout,"%d. ",++lncount);
+	}
+	else {
+	    fputc(i,fout);
+	    i = fgetc(fin);
 	}
     }
     return 1;
+}
+
+int fwordcounter(char const *finname) {
+    //Open files
+    FILE *fin = fopen(finname,"r");
+    
+    //File(s) not found
+    if ((fin == NULL)) return -1;
+      
+    //Word length
+    int wlength = 0;
+    int wcount = 0;
+    int i=fgetc(fin);
+    while (!feof(fin)) {
+	if (isalnum(i)) wlength++;
+	else if (wlength > 0) { 
+	    wcount++;
+	    wlength = 0;
+	}
+	i=fgetc(fin);		
+    }
+    
+    return wcount;
 }
 
 int main(int argc, char *argv[]) {
@@ -55,15 +85,18 @@ int main(int argc, char *argv[]) {
 /*
     //Test file name
     printf ("%s\n%s\n",inFileName,outFileName);
-*/
-
-/*
+    
     //File not found
     if (!(fcopy(inFileName,outFileName))) printf ("Not Found\n");
 */
     
     //File copy with line counter
     if (!(fcopyln(inFileName,outFileName))) printf ("Not Found\n");
+
+    //Counting words
+    int fwcount = fwordcounter(inFileName);
+    if (fwcount == -1) printf ("File Not Found\n");
+    else printf ("%d\n",fwcount);
 
     return 0;
 }
